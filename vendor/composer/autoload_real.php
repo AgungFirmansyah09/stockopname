20b6eba5ad4f03195261a136c03eabd2ce8fb408
Agung Firmansyah <agung.firmansyah@adf.co.id>
2025-12-18 09:55:33 +0700

    commposer instal

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit7eed582ed4210edda26add2561bccb99
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit7eed582ed4210edda26add2561bccb99', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit7eed582ed4210edda26add2561bccb99', 'loadClassLoader'));
